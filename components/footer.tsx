ae9fb1e1df0d67d3bb14f862a5f593c447ac766a
qawsedrftg2301 <pic60900@gmail.com>
2026-06-04 09:52:43 -0300

    Remove Next.js app, use static HTML

--- components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="bg-primary text-primary-foreground py-2 px-3 text-center text-xs">
-      Mini-Split Install • New Brunswick
-    </footer>
-  )
-}
